3ce346dc523f6e566674588c1a80be834ee94abf
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-02-24 15:19:46 +0900

    1st version

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import styles from './App.module.css';
+import { FormControl, List, TextField } from '@material-ui/core';
+import AddToPhotosIcon from "@material-ui/icons/AddToPhotos"
+import { makeStyles } from "@material-ui/styles"
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import { db, auth } from './firebase';
+import TaskItem from "./TaskItem";
 
-function App() {
+
+const useStyles = makeStyles({
+  field: {
+    marginTop: 30,
+    marginBottom: 20,
+  },
+  list: {
+    margin: "auto",
+    width: "40%",
+  },
+});
+
+const App: React.FC = (props:any) => {
+  const [tasks, setTasks] = useState([{ id: '', title: '' }]);
+  const [input, setInput] = useState("")
+  const classes = useStyles();
+  useEffect(() => {
+    const unSub = auth.onAuthStateChanged((user) => {
+      !user && props.history.push('login');
+    });
+    return ()=> unSub()
+})
+
+  useEffect(() => {
+    const unSub = db.collection("tasks").onSnapshot((snapshot) => {
+      setTasks(
+        snapshot.docs.map((doc) => ({ id: doc.id, title: doc.data().title }))
+      );
+    });
+    return () => unSub();
+  }, []);
+
+  const newTask = (e: React.MouseEvent<HTMLButtonElement>) => {
+    db.collection("tasks").add({ title: input });
+    setInput("");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={ styles.app__root }>
+      <h1>Todo App by React/Firebase</h1>
+      <button className={styles.app__logout}
+        onClick={async () => {
+          try {
+            await auth.signOut();
+            props.history.push('login');
+          } catch(error) {
+            alert(error.message);
+        }
+      }}>
+<ExitToAppIcon/>
+      </button>
+      <br />
+      <FormControl>
+        <TextField
+          className={classes.field}
+          InputLabelProps={{
+            shrink: true,
+          }}
+          label='New task?'
+          value={input}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setInput(e.target.value)
+          }
+        />
+      </FormControl>
+      <button className={styles.app__icon} disabled={!input} onClick={newTask}>
+        <AddToPhotosIcon />
+      </button>
+      <List className={classes.list}>
+        {tasks.map((task) => (
+          <TaskItem key={task.id} id={task.id} title={task.title} />
+        ))}
+      </List>
     </div>
   );
-}
+};
 
 export default App;
